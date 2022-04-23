7ec5037c146a8743f890266f2776f31fc510ad0b
Misael Cordova <misael.cordova.f@gmail.com>
2022-04-22 21:03:33 -0300

    .

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const expres = require('express');
-const app = expres();
+const express = require('express');
+const app = express();
 const PORT = 3000;
 
+app.get('/', function(req,res){
+    res.sendFile(__dirname +'/index.html')
+})
 
+//app.use('/estatico', express.static('./index.css'))
+app.use(express.static('public/css'))
 app.listen(PORT, () => {
     console.log('run')
 })
