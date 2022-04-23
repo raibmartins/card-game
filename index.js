657768533b3700a8057a461d4a173c4da02daed4
BETHA\rai.martins <raibmartins@betha.com.br>
2022-04-22 21:12:05 -0300

    REDIRECT

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ app.get('/', function(req,res){
     res.sendFile(__dirname +'/index.html')
 })
 
-//app.use('/estatico', express.static('./index.css'))
 app.use(express.static('public/css'))
+
 app.listen(PORT, () => {
     console.log('run')
+})
+
+app.get('/jogar', function(req,res) {
+    res.send('jogar');
+})
+
+app.get('/login', function(req,res) {
+    res.send('login')
 })
