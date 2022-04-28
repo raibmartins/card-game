868f8f2fd3160aa9b3f46e1c87c9968aa17ffbea
BETHA\rai.martins <raibmartins@betha.com.br>
2022-04-27 22:40:12 -0300

    organizando projeto

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const VIEW_PATH = __dirname + '/views'
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(express.static('public/css'));
+app.use(express.static('public/js'));
+
 app.get('/', function(req,res){
-    res.sendFile(__dirname +'/index.html')
+    res.sendFile(VIEW_PATH +'/index.html')
 })
-
-app.use(express.static('public/css'))
-
-
 
 app.get('/jogar', function(req,res) {
-    res.send('jogar');
-})
+    res.sendFile(VIEW_PATH + '/game/game.html');
+});
 
 app.get('/login', function(req,res) {
-    res.sendFile(__dirname +'/login.html')
-})
+    res.sendFile(VIEW_PATH+'/login/login.html')
+});
+
 app.use('/formCadastro', function(req,res){
-    
-    console.log(req.body)
     if(req.body.name =='misael' && req.body.senha ==='123'){
         res.send('deubom')
     }else{
         res.send('usuário ou senha incorreto')
     }
-})
-app.listen(PORT, () => {
-    console.log('run')
-})
+});
+
+
+app.listen(PORT);
