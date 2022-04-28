8a2abda4ca5e3098dbaf57918cff1f1765b06deb
Misael Cordova <misael.cordova.f@gmail.com>
2022-04-27 21:44:47 -0300

    login

--- index.js
+++ index.js
@@ -2,20 +2,32 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.get('/', function(req,res){
     res.sendFile(__dirname +'/index.html')
 })
 
 app.use(express.static('public/css'))
 
-app.listen(PORT, () => {
-    console.log('run')
-})
+
 
 app.get('/jogar', function(req,res) {
     res.send('jogar');
 })
 
 app.get('/login', function(req,res) {
-    res.send('login')
+    res.sendFile(__dirname +'/login.html')
+})
+app.use('/formCadastro', function(req,res){
+    
+    console.log(req.body)
+    if(req.body.name =='misael' && req.body.senha ==='123'){
+        res.send('deubom')
+    }else{
+        res.send('usuário ou senha incorreto')
+    }
+})
+app.listen(PORT, () => {
+    console.log('run')
 })
